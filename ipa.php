25fb2bbf40b67bc2fc116ea244146ab713956b1e
yosheadha-arch <yosheadha@gmail.com>
2026-05-07 13:23:49 +0700

    baru mas

---
+++ ipa.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    /*background*/
+    body {
+        height: 100vh;
+        background: url('') center/cover no-repeat;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    /*card*/
+    .card {
+        background-color: #fff;
+        width: 70%;
+        padding: 40px;
+        border-radius: 15px;
+        box-shadow: 0 10px 30px rgba(0,0,0,0.25);
+        position: relative;
+    }
+    .icon {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        font-size: 40px;
+    }
+    /*judul*/
+    .card h1{
+        font-size:  32px;
+        margin-bottom: 10px
+    }
+
+    .card p{
+        margin-bottom: 20px;
+        font-size: 22px;
+    }
+
+    
+</style>
+</head>
+<body>
+    
+</body>
+</html>
