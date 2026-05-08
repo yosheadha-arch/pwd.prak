b04b94975ef5ba7d0d58cf3153a9db17781ed99a
bagas551 <124250036@student.upnyk.ac.id>
2026-05-08 17:21:22 +0700

    mtk senang

--- beranda.php
+++ beranda.php
@@ -6,7 +6,7 @@
 <title>Pemetaan Siswa</title>
 
 <!-- FONT -->
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
 <style>
 * {
--- tambah.php
+++ tambah.php
@@ -1,7 +1,33 @@
 <?php
-$konek = mysqli_connect("localhost", "root", "", "pemetaan");
+include 'koneksi.php'
 
-if (!$konek) {
-    die("Maaf koneksi gagal: " . mysqli_connect_error());
+if (isset($_POST['simpan'])) {
+    $nama = $_POST['nama'];
+    $nilai = $_POST['nilai'];
+
+    mysqli_query($konek, "INSERT INTO siswa (nama, nilai) VALUES ('$nama','$nilai')");
+    header("Location: beranda.php");
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Tambah Data Siswa</h2>
+
+    <form method="POST">
+        Nama: <br> 
+        <input type="text" name="nama" required><br><br>
+
+        Nilai: <br>
+        <input type="number" name="nilai" required> <br><br>
+
+        <button type="number" name="simpan">Simpan</button>
+    </form>
+</body>
+</html>
