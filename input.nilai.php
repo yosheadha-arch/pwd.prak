be33cd2b01fbd9e01ee20d791e601e3c343e7342
yosheadha-arch <yosheadha@gmail.com>
2026-05-09 18:52:22 +0700

    ipnput

---
+++ input.nilai.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    body{
+        background: url('gambar1.jpg') no-repeat center center;
+        background-size: cover;
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .container{
+        background: rgba(255,255,255,0.9);
+        padding: 30px;
+        border-radius: 15px;
+        width: 80%;
+        max-width: 1200px;
+        text-align: center;
+    }
+
+    h2 {
+        margin-bottom: 20px;
+    }
+
+    .semester-btn{
+        margin-button: 20px;
+    }
+
+    .semester-btn button {
+        padding: 10px 20px;
+        border: none;
+        margin: 5px;
+        border-radius: 10px;
+        cursor: pointer;
+        background: #ddd;
+    }
+
+    .semester-btn .active{
+        background: #3498db;
+        color: white;
+    }
+
+    .nilai-grid {
+        display: grid;
+    }
+
+</style>
+</head>
+<body>
+    
+</body>
+</html>
